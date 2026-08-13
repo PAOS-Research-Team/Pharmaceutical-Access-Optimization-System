0441020853e98ef35e2204ce8d21791d15c1514a
AMOS-GUY <guyamos28@gmail.com>
2026-08-14 00:28:35 +0800

    Report on the research gaps into the research-gaps-AMOS-GUY.md file

--- 6_hardware/src/IngestionPoller.cpp
+++ 6_hardware/src/IngestionPoller.cpp
@@ -4,29 +4,33 @@
 #include <chrono>
 #include <iostream>
 
-IngestionPoller::IngestionPoller(WearableDevice& device,
-                                  std::string device_id,
-                                  std::function<void(const WearableReading&)> on_reading,
-                                  int interval_seconds)
+IngestionPoller::IngestionPoller(WearableDevice &device,
+                                 std::string device_id,
+                                 std::function<void(const WearableReading &)> on_reading,
+                                 int interval_seconds)
     : device_(device),
       device_id_(std::move(device_id)),
       on_reading_(std::move(on_reading)),
       interval_seconds_(interval_seconds) {}
 
-void IngestionPoller::run(int iterations) {
+void IngestionPoller::run(int iterations)
+{
     device_.connect();
 
-    for (int i = 0; iterations < 0 || i < iterations; ++i) {
+    for (int i = 0; iterations < 0 || i < iterations; ++i)
+    {
         WearableReading reading{
             device_id_,
             device_.read_heart_rate(),
             device_.read_temperature(),
-            std::chrono::system_clock::now()
-        };
+            std::chrono::system_clock::now()};
 
-        if (reading.is_within_normal_range()) {
+        if (reading.is_within_normal_range())
+        {
             on_reading_(reading);
-        } else {
+        }
+        else
+        {
             std::cout << "[hardware] Discarded out-of-range reading\n";
         }
 
